6d3eafad4fd28229163d415dc4266a56ab40f0ce
Tran Duy Hung <duyhungfake@gmail.com>
2026-07-07 20:02:31 +0700

    first commit

--- BUOI1/src/Main.java
+++ BUOI1/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-     System.out.println("Hello");
+    System.out.println("Than em thoi trang phan em tron");
+    System.out.println("Bay noi ba chim may nuoc non");
+    System.out.println("Ran nat mac dau tay ke nan");
+     System.out.println("Nhung em van giu tam long son");
     }
 }
